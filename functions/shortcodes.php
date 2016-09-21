ee3f8af415c180ce3b0c9b3a3366f89fbb2a6d88
Keenan Flogerzi <keenan@keeneyemedia.com>
2016-09-21 12:14:50 -0700

    Add 'kem-element' shortcode
    Adds customizable html generator shortcode

--- functions/shortcodes.php
+++ functions/shortcodes.php
@@ -23,6 +23,45 @@ function divider_shortcode( $atts, $content = null ) {
 add_shortcode( 'divider', 'divider_shortcode' );
 
 
+/** KEM ELEMENT
+-customizable html generator shortcode
+**/
+function kem_element_shortcode( $atts, $content = null ) {
+	extract( shortcode_atts( array(
+		'id' => '',
+		'class' => '',
+		'name' => '',
+		'type' => '',
+	), $atts ) );
+	
+	$id = esc_attr($id);
+	$class = esc_attr($class);
+	$name = esc_attr($name);
+	$type = esc_attr($type);
+	if (!$type) {
+		$type = 'div';
+	}
+	
+	$output = '';
+	$output .= '<' . $type;
+	if ($id) {
+		$output .= ' id="' . $id . '"';
+	}
+	if ($class) {
+		$output .= ' class="' . $class . '"';
+	}
+	if ($name) {
+		$output .= ' name="' . $name . '"';
+	}
+	$output .= '>';
+	$output .= do_shortcode($content);
+	$output .= '</' . $type . '>';
+	
+	return $output;
+}
+add_shortcode('kem-element','kem_element_shortcode');
+
+
 /*STRIP EXTRA 'P' TAGS FROM SHORTCODES*/
 function wpex_fix_shortcodes($content){   
 	$array = array (
